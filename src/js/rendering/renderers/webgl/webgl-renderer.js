53ffc8c617f1b5b154c4d995404aef7a474ba473
Sascha Gehlich <sascha@gehlich.us>
2015-12-17 08:44:15 +0100

    :bug: Don't flip textures

--- src/js/rendering/renderers/webgl/webgl-renderer.js
+++ src/js/rendering/renderers/webgl/webgl-renderer.js
@@ -182,7 +182,6 @@ export default class WebGLRenderer extends BaseRenderer {
     gl.bindTexture(gl.TEXTURE_2D, glTexture)
 
     gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true)
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.getSource())
 
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
