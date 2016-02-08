3574ebc6c1b54c31de2c987ce62f85ca84eafe5c
Sascha Gehlich <sascha@gehlich.us>
2016-02-08 10:34:17 +0100

    :sparkles: Code style check on release

--- gulp/release.js
+++ gulp/release.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var gulpSequence = require('gulp-sequence')
 
 gulp.task('release', function (cb) {
-  var tasks = ['assets', 'sass', 'webpack', 'uglify:js', 'uglify:css']
+  var tasks = ['assets', 'sass', 'standard', 'webpack', 'uglify:js', 'uglify:css']
   tasks.push(cb)
   gulpSequence.apply(this, tasks)
 })
