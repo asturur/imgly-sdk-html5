218c81a47922553bca99f1347a4f858b6448c95f
Sascha Gehlich <sascha@gehlich.us>
2016-02-19 15:00:37 +0100

    :bug: WebGL: Use nearest neighbor rendering

--- src/js/sdk/engine/utils/render-target.js
+++ src/js/sdk/engine/utils/render-target.js
@@ -136,8 +136,8 @@ export default class RenderTarget {
     // Set scale and repeat parameters
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 
     // Bind texture to FBO
     gl.bindFramebuffer(gl.FRAMEBUFFER, this._framebuffer)
