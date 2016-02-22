eb649ae8565af1b6f5515fe328ebe5262054932b
Sascha Gehlich <sascha@gehlich.us>
2016-02-22 07:10:51 +0100

    :memo: Basic API documentation setup

---
+++ gulp/jsdoc.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp')
+var jsdoc = require('gulp-jsdoc3')
+var handleErrors = require('./util/handleErrors')
+
+gulp.task('jsdoc', function () {
+  return gulp.src([
+    './src/js/sdk/**/*.js',
+    './src/js/night-react-ui/**/*.js',
+    '!./src/js/sdk/vendor/**/*.js'
+  ])
+    .pipe(jsdoc({
+      tags: {
+        allowUnknownTags: true
+      },
+      source: {
+        excludePattern: '(^|\\/|\\\\)_'
+      },
+      opts: {
+        destination: './doc',
+        template: 'node_modules/jaguarjs-jsdoc'
+      },
+      plugins: [
+        'plugins/markdown'
+      ],
+      templates: {
+        cleverLinks: false,
+        monospaceLinks: false,
+        default: {
+          outputSourceFiles: true,
+          layoutFile: 'node_modules/jaguarjs-jsdoc/tmpl/layout.tmpl'
+        },
+        theme: 'cerulean',
+        navType: 'vertical',
+        linenums: true,
+        dateFormat: 'MMMM Do YYYY, h:mm:ss a'
+      }
+    }
+, () => {}))
+    .on('error', handleErrors)
+})
