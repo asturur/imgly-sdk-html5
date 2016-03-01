e65a6a5fbd2220a2ecec2e7c224b7f4fecf7816a
Sascha Gehlich <sascha@gehlich.us>
2016-03-01 14:11:58 +0100

    :bug: RenderTarget: Use Linear as min filter

--- src/js/sdk/engine/utils/render-target.js
+++ src/js/sdk/engine/utils/render-target.js
@@ -149,7 +149,7 @@ class RenderTarget {
     // Set scale and repeat parameters
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 
     // Bind texture to FBO
