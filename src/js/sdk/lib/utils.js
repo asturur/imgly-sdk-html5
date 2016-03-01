9367dc907c25336bb0027eb6bd759397007466ad
Sascha Gehlich <sascha@gehlich.us>
2016-03-01 10:50:51 +0100

    :fire: Remove SDKUtils#select

--- src/js/sdk/lib/utils.js
+++ src/js/sdk/lib/utils.js
@@ -28,56 +28,6 @@ class Utils {
   }
 
   /**
-   * Returns the items selected by the given selector
-   * @param  {Array} items
-   * @param  {PhotoEditorSDK~Selector} selector = null - The selector
-   * @return {Array} The selected items
-   */
-  static select (items, selector = null) {
-    if (selector === null) {
-      return items
-    }
-
-    // Turn string parameter into an array
-    if (typeof selector === 'string') {
-      selector = selector.split(',').map(function (identifier) {
-        return identifier.trim()
-      })
-    }
-
-    // Turn array parameter into an object with `only`
-    if (Utils.isArray(selector)) {
-      selector = { only: selector }
-    }
-
-    if (typeof selector.only !== 'undefined') {
-      if (typeof selector.only === 'string') {
-        selector.only = selector.only.split(',').map(function (identifier) {
-          return identifier.trim()
-        })
-      }
-
-      // Select only the given identifiers
-      return items.filter(function (item) {
-        return selector.only.indexOf(item) !== -1
-      })
-    } else if (typeof selector.except !== 'undefined') {
-      if (typeof selector.except === 'string') {
-        selector.except = selector.except.split(',').map(function (identifier) {
-          return identifier.trim()
-        })
-      }
-
-      // Select all but the given identifiers
-      return items.filter(function (item) {
-        return selector.except.indexOf(item) === -1
-      })
-    }
-
-    throw new Error('Utils#select failed to filter items.')
-  }
-
-  /**
    * Returns the given object's values as an array
    * @param {Object} object
    * @returns {Array<*>}
