5fceaae99e85506cb97e39647d2d31cb814a1505
Sascha Gehlich <sascha@gehlich.us>
2016-03-01 11:46:44 +0100

    :sparkles: Add all default stickers back in

--- src/js/night-react-ui/lib/constants.js
+++ src/js/night-react-ui/lib/constants.js
@@ -56,6 +56,262 @@ export default {
             'height': 91
           }
         }
+      },
+      {
+        'name': 'glasses-normal',
+        'label': 'Normal glasses',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/glasses-normal.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/glasses-normal.png',
+            'width': 249,
+            'height': 87
+          }
+        }
+      },
+      {
+        'name': 'glasses-shutter-green',
+        'label': 'Green shutter glasses',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/glasses-shutter-green.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/glasses-shutter-green.png',
+            'width': 235,
+            'height': 89
+          }
+        }
+      },
+      {
+        'name': 'glasses-shutter-yellow',
+        'label': 'Yellow shutter glasses',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/glasses-shutter-yellow.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/glasses-shutter-yellow.png',
+            'width': 236,
+            'height': 89
+          }
+        }
+      },
+      {
+        'name': 'glasses-sun',
+        'label': 'Sun glasses',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/glasses-sun.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/glasses-sun.png',
+            'width': 250,
+            'height': 87
+          }
+        }
+      },
+      {
+        'name': 'hat-cap',
+        'label': 'Cap',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/hat-cap.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/hat-cap.png',
+            'width': 237,
+            'height': 189
+          }
+        }
+      },
+      {
+        'name': 'hat-cylinder',
+        'label': 'Cylinder',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/hat-cylinder.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/hat-cylinder.png',
+            'width': 225,
+            'height': 176
+          }
+        }
+      },
+      {
+        'name': 'hat-party',
+        'label': 'Party hat',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/hat-party.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/hat-party.png',
+            'width': 140,
+            'height': 226
+          }
+        }
+      },
+      {
+        'name': 'hat-sheriff',
+        'label': 'Sheriff hat',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/hat-sheriff.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/hat-sheriff.png',
+            'width': 251,
+            'height': 129
+          }
+        }
+      },
+      {
+        'name': 'heart',
+        'label': 'Heart',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/heart.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/heart.png',
+            'width': 185,
+            'height': 174
+          }
+        }
+      },
+      {
+        'name': 'mustache-long',
+        'label': 'Long mustache',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/mustache-long.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/mustache-long.png',
+            'width': 199,
+            'height': 182
+          }
+        }
+      },
+      {
+        'name': 'mustache1',
+        'label': 'Brown mustache',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/mustache1.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/mustache1.png',
+            'width': 238,
+            'height': 106
+          }
+        }
+      },
+      {
+        'name': 'mustache2',
+        'label': 'Black mustache',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/mustache2.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/mustache2.png',
+            'width': 250,
+            'height': 82
+          }
+        }
+      },
+      {
+        'name': 'mustache3',
+        'label': 'Brown mustache',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/mustache3.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/mustache3.png',
+            'width': 250,
+            'height': 94
+          }
+        }
+      },
+      {
+        'name': 'pipe',
+        'label': 'Pipe',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/pipe.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/pipe.png',
+            'width': 240,
+            'height': 112
+          }
+        }
+      },
+      {
+        'name': 'snowflake',
+        'label': 'Snow flake',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/snowflake.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/snowflake.png',
+            'width': 204,
+            'height': 171
+          }
+        }
+      },
+      {
+        'name': 'star',
+        'label': 'Star',
+        'images': {
+          'mediaThumb': {
+            'uri': 'stickers/thumb/star.png',
+            'width': 100,
+            'height': 100
+          },
+          'mediaBase': {
+            'uri': 'stickers/base/star.png',
+            'width': 201,
+            'height': 191
+          }
+        }
       }
     ]
   }
